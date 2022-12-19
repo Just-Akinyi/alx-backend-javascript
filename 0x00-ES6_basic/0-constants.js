a67f57e3a5ed44ccffc8ba2a3c78f0db5b6ec2ad
Just-Akinyi <justakinyi1@gmail.com>
2022-12-20 01:49:33 +0300

    new

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,13 @@
-const taskFirst = () =>{
-    var task = 'I prefer const when I can.';
-    return task;
-}
-let getLast = () => {
-    return ' is okay';
-  }
-let taskNext = () => {
-    var combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-}
+const taskFirst = () => {
+  const task = 'I prefer const when I can.';
+  return task;
+};
+const getLast = () => ' is okay';
+const taskNext = () => {
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-export {taskFirst,taskNext} 
+  return combination;
+};
+
+export { taskFirst, taskNext };
