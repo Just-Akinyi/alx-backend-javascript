626723a1beedb6fe6c9cbe514a3be3a0477ccae4
Just-Akinyi <justakinyi1@gmail.com>
2022-12-20 22:30:04 +0300

    ES6 reconstruction

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,13 +1,3 @@
-const getResponseFromAPI = new Promise((resolve, reject) => {
-    resolve('Success!');
-  });
-  
-// getResponseFromAPI.then((value) => {
-//     console.log(value);
-//     // expected output: "Success!"
-//   });
-//   getResponseFromAPI.catch((error) => {
-//     console.log(error);
-//     // expected output: "Success!"
-//   });
-module.exports = {getResponseFromAPI}
+const getResponseFromAPI = new Promise();
+
+module.exports = { getResponseFromAPI };
