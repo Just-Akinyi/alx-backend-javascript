9ffc1df3e8788b092c03e0d74a9c407751a1e197
Just-Akinyi <justakinyi1@gmail.com>
2022-12-22 00:35:53 +0300

    eslint

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,2 +1,9 @@
-function signUpUser(firstName, lastName) {
+export default function signUpUser(firstName, lastName) {
+    return new Promise((resolve,reject)=>{
+        resolve({
+            firstName: firstName,
+            lastName: lastName,
+          }
+          )
+    })
 }
