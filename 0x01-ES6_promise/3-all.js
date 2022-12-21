79d566d9f95f2775f5fd14bfd504eddabe958fb0
Just-Akinyi <justakinyi1@gmail.com>
2022-12-21 22:40:56 +0300

    Promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import {uploadPhoto, createUser} from './utils'
+export default function handleProfileSignup(){
+    Promise.all([
+        uploadPhoto,
+        createUser
+    ]).then((message)=>{
+        console.log({
+            body,
+            firstName,
+            lastName
+        })
+    }).catch(()=>{
+        console.log('Signup system offline')
+    })
+}
