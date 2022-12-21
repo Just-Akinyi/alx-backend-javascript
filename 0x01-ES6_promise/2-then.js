1326e2d4413e7a7719878073e8834786a6531c09
Just-Akinyi <justakinyi1@gmail.com>
2022-12-21 22:16:46 +0300

    Promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,32 @@
-export default function getResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
+// export default function handleResponseFromAPI(promise){
+//   return new Promise((resolve,reject)=>{
+//     if(promise){
+//       resolve({
+//         status: 200,
+//         body: 'success'
+//       })
+//     }else{
+//       reject(new Error('please'))
+//     }
+//   })
+// }
+// const res = handleResponseFromAPI()
+// res.then(()=>{
+//   message = 'Got a response from the API'
+//   console.log(message)
+// }).catch((message)=> message)
+
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
         status: 200,
-        body: 'Success',
-      })
-        .then(() => {
-          console.log('Got a response from the API');
-        });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+        body: 'success',
+      };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
