485a305deca78ed30588b1b7ba6d4d077fb5601c
Just-Akinyi <justakinyi1@gmail.com>
2022-12-21 23:58:12 +0300

    eslint

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    const num = 3 + 2;
-    if (num === 5) {
-      resolve('Success');
-    } else {
-      reject(new Error('Failed'));
-    }
-  });
+  return new Promise((resolve, reject) => [resolve, reject]);
 }
